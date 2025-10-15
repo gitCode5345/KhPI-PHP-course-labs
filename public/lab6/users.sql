3636fc7462f60d7f392439ed5b4f571770d70e9c
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-10-15 21:02:45 +0300

    Requirement for completing the task: add sql dump

---
+++ public/lab6/users.sql
@@ -0,0 +1,24 @@
+-- Adminer 4.8.1 MySQL 9.4.0 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1,	'user',	'mail@mail.com',	'ed2b1f468c5f915f3f1cf75d7068baae');
+
+-- 2025-10-15 17:51:18
